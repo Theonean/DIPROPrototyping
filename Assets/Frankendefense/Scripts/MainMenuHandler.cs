b38a2248d7da280fc0498c89135a31394c1db5cc
Robin <robinwelmers@protonmail.com>
2024-09-24 16:52:25 +0200

    added main menu UI with baseline functionality to jump into game prototypes

---
+++ Assets/Frankendefense/Scripts/MainMenuHandler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
