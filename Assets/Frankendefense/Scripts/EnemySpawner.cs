b347f901740a4e0a507f0cda983f275367b28c56
Robin <robinwelmers@protonmail.com>
2024-09-27 20:44:37 +0200

    added simple enemy spawner

--- Assets/Frankendefense/Scripts/EnemySpawner.cs
+++ Assets/Frankendefense/Scripts/EnemySpawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject EnemyPrefab;
+    public float SpawnRate = 1f;
+    public float SpawnRadius = 5f; //Visualize with gizmo
+    private float m_SpawnTimer = 0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        m_SpawnTimer += Time.deltaTime;
+        if (m_SpawnTimer >= SpawnRate)
+        {
+            m_SpawnTimer = 0f;
+            SpawnEnemy();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, SpawnRadius);
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 spawnPosition = transform.position + Random.insideUnitSphere * SpawnRadius;
+        spawnPosition.y = 0;
+        Instantiate(EnemyPrefab, spawnPosition, Quaternion.identity);
     }
 }
