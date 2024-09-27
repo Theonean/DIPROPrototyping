8e8fae057ee4b92fecd3d5c1ba770aad49147da8
Robin <robinwelmers@protonmail.com>
2024-09-27 20:45:44 +0200

    added camera shake for when a enemy reaches player and they should take damage

--- Assets/Frankendefense/Scripts/PlayerCore.cs
+++ Assets/Frankendefense/Scripts/PlayerCore.cs
@@ -7,7 +7,15 @@ public class PlayerCore : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public UnityEvent returnLegs;
-    
+    Camera m_Camera;
+    Vector3 m_OriginalCameraPosition;
+
+    private void Awake()
+    {
+        m_Camera = Camera.main;
+        m_OriginalCameraPosition = m_Camera.transform.localPosition;
+    }
+
 
     //When spacebar is pressed in update, call the returnLegs event
     void Update()
@@ -37,5 +45,35 @@ public class PlayerCore : MonoBehaviour
         {
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
         }
+    }
+
+    //When colliding with an enemy, destroy enemy
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            StartCoroutine(ShakeScreen(0.5f, 0.005f));
+            Debug.Log("Player should Take Damage");
+        }
+    }
+
+    private IEnumerator ShakeScreen(float duration, float magnitude)
+    {
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float z = Random.Range(-1f, 1f) * magnitude;
+
+            m_Camera.transform.localPosition = new Vector3(x, m_OriginalCameraPosition.y, z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        m_Camera.transform.localPosition = m_OriginalCameraPosition;
     }
 }
