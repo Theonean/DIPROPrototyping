9931bbbbfad4ceacee5a3587fd4cd6a2ad693cf9
Robin <robinwelmers@protonmail.com>
2024-09-27 16:41:59 +0200

    Duplicated enemies in scene and created empty scripts

---
+++ Assets/Frankendefense/Scripts/DamageHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour
+{
+    public string EnemyTag;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
