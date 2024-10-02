21048e446a105f468fa3255e8f4a7bd9b59f798b
Robin <robinwelmers@protonmail.com>
2024-10-02 13:27:22 +0200

    Added damage effect to player, particle system to enemy death and a you died screen with prompt to reload ~ PLAYTEST VERSION 1

--- Assets/Frankendefense/Scripts/PlayerCore.cs
+++ Assets/Frankendefense/Scripts/PlayerCore.cs
@@ -12,12 +12,17 @@ public class PlayerCore : MonoBehaviour
     Vector3 m_OriginalCameraPosition;
     public Slider healthSlider;
 
+    MeshRenderer m_Renderer;
+
     public UnityEvent PlayerDeath;
+    Coroutine m_damageEffectCoroutine;
 
     private void Awake()
     {
         m_Camera = Camera.main;
         m_OriginalCameraPosition = m_Camera.transform.localPosition;
+        m_Renderer = GetComponent<MeshRenderer>();
+        m_Renderer.material.color = Color.white;
     }
 
 
@@ -54,10 +59,19 @@ public class PlayerCore : MonoBehaviour
     //When colliding with an enemy, destroy enemy
     private void OnTriggerEnter(Collider other)
     {
+        //If the object is an enemy, destroy the enemy and reduce health
         if (other.gameObject.tag == "Enemy")
         {
+            //Destroy enemy and start damage effect
             Destroy(other.gameObject);
+
+            if (m_damageEffectCoroutine != null)
+                StopCoroutine(m_damageEffectCoroutine);
+                
+            m_damageEffectCoroutine = StartCoroutine(DamageEffect());
+
             healthSlider.value -= 1;
+            //If health is less than or equal to 0, player has died
             if (healthSlider.value <= 0)
             {
                 //Inform to all listeners that the player has died to allow for a manual resetting of scene instead of reloading the scene
@@ -76,6 +90,19 @@ public class PlayerCore : MonoBehaviour
                 //Return health back to full
                 healthSlider.value = 3;
             }
+        }
+    }
+
+    //When player takes damage, turn the body red and fade back to white over time
+    IEnumerator DamageEffect()
+    {
+        m_Renderer.material.color = Color.red;
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            m_Renderer.material.color = Color.Lerp(Color.red, Color.white, t);
+            yield return null;
         }
     }
 }
