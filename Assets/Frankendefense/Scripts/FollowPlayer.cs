fc6c097195284e28a3e60f99e4d05db33100352a
Robin <robinwelmers@protonmail.com>
2024-10-09 17:12:51 +0200

    Added knockback to enemies

--- Assets/Frankendefense/Scripts/FollowPlayer.cs
+++ Assets/Frankendefense/Scripts/FollowPlayer.cs
@@ -10,10 +10,11 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-
     GameObject m_ControlZone;
     [SerializeField]
-    float m_MoveSpeed = 16f;
+    float m_MoveSpeed = 20f;
+    bool m_IsMoving = true;
+    public AnimationCurve knockbackCurve;
 
     // Start is called before the first frame update
     void Start()
@@ -24,16 +25,19 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(m_ControlZone.transform);
+        if (m_IsMoving)
+        {
+            transform.LookAt(m_ControlZone.transform);
 
-        //If enemy is seen by camera.main or close to zone move normal speed otherwise move 5x speed
-        if (IsVisibleToCamera() || Vector3.Distance(transform.position, m_ControlZone.transform.position) < 35f)
-        {
-            transform.position += transform.forward * m_MoveSpeed * Time.deltaTime;
-        }
-        else
-        {
-            transform.position += transform.forward * (m_MoveSpeed * 5f) * Time.deltaTime;
+            //If enemy is seen by camera.main or close to zone move normal speed otherwise move 5x speed
+            if (IsVisibleToCamera() || Vector3.Distance(transform.position, m_ControlZone.transform.position) < 35f)
+            {
+                transform.position += transform.forward * m_MoveSpeed * Time.deltaTime;
+            }
+            else
+            {
+                transform.position += transform.forward * (m_MoveSpeed * 10f) * Time.deltaTime;
+            }
         }
 
 
@@ -43,5 +47,19 @@ public class FollowPlayer : MonoBehaviour
     {
         Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
         return GeometryUtility.TestPlanesAABB(planes, GetComponentInChildren<Collider>().bounds);
+    }
+
+    public IEnumerator ApplyKnockback(Vector3 direction, float knockback)
+    {
+        m_IsMoving = false;
+        float timer = 0f;
+        float knockbackTime = 0.5f;
+        while (timer < knockbackTime)
+        {
+            transform.position += direction * knockback * Time.deltaTime * knockbackCurve.Evaluate(timer / knockbackTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        m_IsMoving = true;
     }
 }
