c40b9b49a9bb4b4789b88fc9b98e113bc46463c4
Robin <robinwelmers@protonmail.com>
2024-10-15 17:59:15 +0200

    Consolidated enemy prefabs into one which decides it's "type", to reduce error-rate when adjusting enemy prefab and having to mirror changes

---
+++ Assets/EnemyTypeDecider.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTypeDecider : MonoBehaviour
+{
+    //This Class decides what type of enemy the gameobject should represent
+    public GameObject enemyRegularModel;
+    public GameObject enemyFastModel;
+    void Start()
+    {
+        //Get the followplayer script
+        FollowPlayer followPlayer = GetComponent<FollowPlayer>();
+
+        bool random = Random.value > 0.5f;
+
+        //regular enemy
+        if (random)
+        {
+            enemyRegularModel.SetActive(true);
+            enemyFastModel.SetActive(false);
+            followPlayer.m_MoveSpeed = 4f;
+        }
+        //Fast enemy
+        else
+        {
+            enemyFastModel.SetActive(true);
+            enemyRegularModel.SetActive(false);
+            followPlayer.m_MoveSpeed = 6f;
+        }
+
+    }
+}
