f871598e770dff6ed04fa2a00b25c44907684afe
ManuelIni <113027541+ManuelIni@users.noreply.github.com>
2024-10-29 16:46:36 +0100

    implemented obstacle explosion effect

--- Assets/Frankendefense/Scripts/Obstacle.cs
+++ Assets/Frankendefense/Scripts/Obstacle.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Obstacle : MonoBehaviour
 {
     public string[] destructiveTags;
     public SkinnedMeshRenderer meshRenderer;
+
+    public VisualEffect ExplosionEffect;
 
     private void Start()
     {
@@ -36,7 +40,21 @@ public class Obstacle : MonoBehaviour
         }
         else if (destructiveTags.Contains(other.gameObject.tag))
         {
-            Destroy(gameObject);
+            HandleExplosion();
         }
+    }
+
+    private void HandleExplosion()
+    {
+        ExplosionEffect.Play();
+        StartCoroutine(DestroyAfterDelay(2f));
+    }
+
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(0.2f);
+        meshRenderer.enabled = false;
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
     }
 }
