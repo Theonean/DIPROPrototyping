9c4d34d506cc39e41ef6db9f0deef1f712c05a4d
Robin <robinwelmers@protonmail.com>
2024-10-30 17:16:19 +0100

    Lengthened GameOver UI FadeIn, changed Background color

--- Assets/Frankendefense/Scripts/FrankenGameManager.cs
+++ Assets/Frankendefense/Scripts/FrankenGameManager.cs
@@ -79,7 +79,7 @@ public class FrankenGameManager : MonoBehaviour
     IEnumerator ScaleUpUI(CanvasGroup uiOverlay)
     {
         float time = 0f;
-        float maxTime = 5f;
+        float maxTime = 25f;
         while (time < maxTime)
         {
             time += Time.deltaTime;
