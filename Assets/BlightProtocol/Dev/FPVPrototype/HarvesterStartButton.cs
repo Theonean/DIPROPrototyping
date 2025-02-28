7be141aefda50d30b745045e47ed4012b22348f2
ManuelIni <113027541+ManuelIni@users.noreply.github.com>
2025-02-28 14:10:34 +0100

    adds smoke trail as POI (not interactable), changes stop button to three speed buttons,

--- Assets/BlightProtocol/Dev/FPVPrototype/HarvesterStartButton.cs
+++ Assets/BlightProtocol/Dev/FPVPrototype/HarvesterStartButton.cs
@@ -4,26 +4,11 @@ using UnityEngine;
 
 public class HarvesterStartButton : FPVInteractable
 {
-    private bool isMoving = true;
-    private float harvesterSpeed = 0f;
 
-    void Start()
-    {
-        harvesterSpeed = ControlZoneManager.moveSpeed;
-    }
+    public float setSpeed = 0f;
+
     public override void Interact()
     {
-        Debug.Log("Harvester Start Button Pressed");
-        if (isMoving)
-        {
-            ControlZoneManager.moveSpeed = 0f;
-            isMoving = false;
-        }
-        else
-        {
-            ControlZoneManager.moveSpeed = harvesterSpeed;
-            isMoving = true;
-        }
-        Debug.Log(ControlZoneManager.moveSpeed);
+        ControlZoneManager.moveSpeed = setSpeed;
     }
 }
