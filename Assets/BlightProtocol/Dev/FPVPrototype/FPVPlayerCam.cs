a7bf6548e826aef0f8d3f41f4c4a7ec1c55b52fe
ManuelIni <113027541+ManuelIni@users.noreply.github.com>
2025-03-12 23:13:30 +0100

    enables rb interpolation and smoother camera

--- Assets/BlightProtocol/Dev/FPVPrototype/FPVPlayerCam.cs
+++ Assets/BlightProtocol/Dev/FPVPrototype/FPVPlayerCam.cs
@@ -12,14 +12,17 @@ public class FPVPlayerCam : MonoBehaviour
     float xRotation;
     float yRotation;
 
-    private void Update() {
+    private void Update()
+    {
         float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
+    }
+    private void LateUpdate()
+    {
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.localRotation = Quaternion.Euler(0, yRotation, 0);
     }
