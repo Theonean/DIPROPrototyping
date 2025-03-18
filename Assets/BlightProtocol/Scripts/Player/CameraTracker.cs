051a3fccd3cdfb5d9a00cac1a2a50bdf66b40cda
Robin <robinwelmers@protonmail.com>
2025-03-18 13:30:10 +0100

    Removed OutOfRange Handling from Camera Tracker, Camera Tracker now placed on DroneCamera in Main Scene
    Drone Camera used to be placed onto the player, for some unholy reasong

--- Assets/BlightProtocol/Scripts/Player/CameraTracker.cs
+++ Assets/BlightProtocol/Scripts/Player/CameraTracker.cs
@@ -1,29 +1,16 @@
-using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraTracker : MonoBehaviour
 {
     public static CameraTracker Instance { get; private set; }
-    public bool trackObjectWithCamera = false;
     public GameObject objectToTrack;
-    public GameObject player;
-    public GameObject controlZone;
-    public SpriteRenderer arrowSprite;
     public AnimationCurve cameraFollowCurve;
-    public float maxDistanceFromHarvester;
     public float cameraMoveSpeed = 20f;
-    public GameObject OutOfRangeUIGroup;
-    public CanvasGroup outOfRangeUIGroupCanvas; // CanvasGroup for controlling the alpha of the UI
-    public TextMeshProUGUI countdownUntilRespawnText;
-    public float countdownUntilRespawnTime;
-    private float m_RespawnTimer = 0f;
-    private bool m_PlayerInRange = true;
     float m_MaxCameraDistance = 2f;
     private Vector3 cameraOffset;
-    private PlayerCore playerCore;
 
-    public Camera topDownCamera;
-    private const float fadeStartDistance = 20f; // Start fading when player is this close to the max distance
+    private Camera topDownCamera;
 
     private void Awake()
     {
@@ -35,36 +22,21 @@ public class CameraTracker : MonoBehaviour
         {
             Instance = this;
         }
+
+        topDownCamera = GetComponent<Camera>();
     }
 
     private void Start()
     {
         // Save the initial offset between camera and object
-        cameraOffset = topDownCamera.transform.position - player.transform.position;
-        playerCore = GetComponentInChildren<PlayerCore>();
-
-    }
-
-    private void Update()
-    {
-        if (!m_PlayerInRange && !ControlZoneManager.Instance.GetZoneState().Equals(ZoneState.DIED))
-        {
-            m_RespawnTimer -= Time.deltaTime;
-            countdownUntilRespawnText.text = m_RespawnTimer.ToString("F2");
-            if (m_RespawnTimer <= 0f)
-            {
-                SetIsPlayerInRange(true);
-                //Make the player die
-                playerCore.ModifyHealth(-100);
-            }
-        }
+        cameraOffset = topDownCamera.transform.position - objectToTrack.transform.position;
     }
 
     void FixedUpdate()
     {
-        if (trackObjectWithCamera && objectToTrack != null)
+        if (objectToTrack != null)
         {
-            Vector3 targetPosition = player.transform.position + cameraOffset;
+            Vector3 targetPosition = objectToTrack.transform.position + cameraOffset;
 
             float distance = Vector3.Distance(topDownCamera.transform.position, targetPosition);
             float t = Mathf.Clamp(distance / m_MaxCameraDistance, 0, 1);
@@ -74,75 +46,6 @@ public class CameraTracker : MonoBehaviour
                 topDownCamera.transform.position,
                 targetPosition,
                 cameraMoveSpeed * Time.fixedDeltaTime * cameraFollowCurve.Evaluate(t));
-        }
-
-        // Rotate arrow to face the control zone, only when the harvester is not dead
-        if (!ControlZoneManager.Instance.GetZoneState().Equals(ZoneState.DIED))
-        {
-            float distanceToControlZone = Vector3.Distance(player.transform.position, controlZone.transform.position);
-
-            // When near the control zone, make the arrow invisible and handle UI fade
-            if (distanceToControlZone < maxDistanceFromHarvester - fadeStartDistance)
-            {
-                SetIsPlayerInRange(true);
-            }
-            else
-            {
-                SetIsPlayerInRange(false);
-            }
-
-            // Fade the UI based on the player's distance from the control zone
-            HandleUIFade(distanceToControlZone);
-        }
-    }
-
-    private void SetIsPlayerInRange(bool isInRange)
-    {
-        // Only run the logic when the player's range status changes
-        //Disable logic when control zone died
-        if (m_PlayerInRange != isInRange)
-        {
-            if (isInRange)
-            {
-                // Player is now in range
-                OutOfRangeUIGroup.SetActive(false);
-                arrowSprite.enabled = false;
-                m_PlayerInRange = true;
-            }
-            else
-            {
-                // Player is out of range
-                OutOfRangeUIGroup.SetActive(true);
-                arrowSprite.enabled = true;
-                m_PlayerInRange = false;
-
-                // Start the respawn timer when the player goes out of range
-                m_RespawnTimer = countdownUntilRespawnTime;
-            }
-        }
-    }
-
-    private void HandleUIFade(float distanceToControlZone)
-    {
-        // Calculate the starting distance for the fade (when to start fading in the UI)
-        float fadeStart = maxDistanceFromHarvester - fadeStartDistance;
-
-        // If the player is within the fade range, start fading in the UI
-        if (distanceToControlZone >= fadeStart && distanceToControlZone <= maxDistanceFromHarvester)
-        {
-            // Calculate the fade factor (0 when just entering fade range, 1 when out of range)
-            float fadeFactor = (distanceToControlZone - fadeStart) / (maxDistanceFromHarvester - fadeStart);
-            outOfRangeUIGroupCanvas.alpha = Mathf.Clamp01(fadeFactor); // Fade UI based on distance
-        }
-        else if (distanceToControlZone > maxDistanceFromHarvester)
-        {
-            // Ensure UI is fully visible when the player is out of range
-            outOfRangeUIGroupCanvas.alpha = 1f;
-        }
-        else
-        {
-            // Ensure UI is hidden when the player is close to the control zone
-            outOfRangeUIGroupCanvas.alpha = 0f;
         }
     }
 }
---
+++ Assets/BlightProtocol/Scripts/Player/OutOfRangeHandler.cs
@@ -0,0 +1,98 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerOutOfRangeHandler : MonoBehaviour
+{
+    public SpriteRenderer arrowSprite;
+    public GameObject OutOfRangeUIGroup;
+    public CanvasGroup outOfRangeUIGroupCanvas;
+    public TextMeshProUGUI countdownUntilRespawnText;
+    public float countdownUntilRespawnTime;
+    public float maxDistanceFromHarvester;
+
+    private float m_RespawnTimer = 0f;
+    private bool m_PlayerInRange = true;
+
+    private const float fadeStartDistance = 20f;
+    private DroneMovement droneMovement;
+    private ControlZoneManager harvester;
+
+    void Start()
+    {
+        droneMovement = GetComponentInChildren<DroneMovement>();
+        harvester = ControlZoneManager.Instance;
+    }
+
+    private void Update()
+    {
+        if (!m_PlayerInRange && !harvester.GetZoneState().Equals(ZoneState.DIED))
+        {
+            m_RespawnTimer -= Time.deltaTime;
+            countdownUntilRespawnText.text = m_RespawnTimer.ToString("F2");
+
+            if (m_RespawnTimer <= 0f)
+            {
+                SetIsPlayerInRange(true);
+                PlayerCore.Instance.ModifyHealth(-100);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!harvester.GetZoneState().Equals(ZoneState.DIED))
+        {
+            float distanceToControlZone = Vector3.Distance(droneMovement.transform.position, harvester.transform.position);
+
+            if (distanceToControlZone < maxDistanceFromHarvester - fadeStartDistance)
+            {
+                SetIsPlayerInRange(true);
+            }
+            else
+            {
+                SetIsPlayerInRange(false);
+            }
+
+            HandleUIFade(distanceToControlZone);
+        }
+    }
+
+    private void SetIsPlayerInRange(bool isInRange)
+    {
+        if (m_PlayerInRange != isInRange)
+        {
+            if (isInRange)
+            {
+                OutOfRangeUIGroup.SetActive(false);
+                arrowSprite.enabled = false;
+                m_PlayerInRange = true;
+            }
+            else
+            {
+                OutOfRangeUIGroup.SetActive(true);
+                arrowSprite.enabled = true;
+                m_PlayerInRange = false;
+                m_RespawnTimer = countdownUntilRespawnTime;
+            }
+        }
+    }
+
+    private void HandleUIFade(float distanceToControlZone)
+    {
+        float fadeStart = maxDistanceFromHarvester - fadeStartDistance;
+
+        if (distanceToControlZone >= fadeStart && distanceToControlZone <= maxDistanceFromHarvester)
+        {
+            float fadeFactor = (distanceToControlZone - fadeStart) / (maxDistanceFromHarvester - fadeStart);
+            outOfRangeUIGroupCanvas.alpha = Mathf.Clamp01(fadeFactor);
+        }
+        else if (distanceToControlZone > maxDistanceFromHarvester)
+        {
+            outOfRangeUIGroupCanvas.alpha = 1f;
+        }
+        else
+        {
+            outOfRangeUIGroupCanvas.alpha = 0f;
+        }
+    }
+}
