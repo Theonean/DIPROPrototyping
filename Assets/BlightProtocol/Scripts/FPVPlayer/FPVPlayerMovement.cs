85bcb9950bf354cb88bd6aed47e591141bed2401
ManuelIni <113027541+ManuelIni@users.noreply.github.com>
2025-03-20 09:54:04 +0100

    fixed fpv player slope movement

--- Assets/BlightProtocol/Scripts/FPVPlayer/FPVPlayerMovement.cs
+++ Assets/BlightProtocol/Scripts/FPVPlayer/FPVPlayerMovement.cs
@@ -11,6 +11,11 @@ public class FPVPlayerMovement : MonoBehaviour
     public LayerMask whatIsGround;
     private bool grounded;
 
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool onSlope;
+
     public Transform orientation;
 
     float horizontalInput;
@@ -19,7 +24,8 @@ public class FPVPlayerMovement : MonoBehaviour
     Vector3 moveDirection;
     Rigidbody rb;
 
-    private void Start() {
+    private void Start()
+    {
         rb = GetComponent<Rigidbody>();
     }
     void Update()
@@ -27,36 +33,81 @@ public class FPVPlayerMovement : MonoBehaviour
         MyInput();
     }
 
-    private void MyInput() {
+    private void MyInput()
+    {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
     }
 
-    private void MovePlayer() {
+    private void MovePlayer()
+    {
+        onSlope = OnSlope();
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (onSlope)
+        {
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+            if (rb.velocity.y > 0)
+            {
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+        }
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        rb.useGravity = !onSlope;
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         if (FPVPlayerCam.Instance.isLocked) return;
         MovePlayer();
         SpeedControl();
 
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-        if (grounded) {
+        if (grounded)
+        {
             rb.drag = groundDrag;
         }
-        else {
+        else
+        {
             rb.drag = 0;
         }
     }
 
-    private void SpeedControl() {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        if (flatVel.magnitude > moveSpeed) {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+    private void SpeedControl()
+    {
+        if (onSlope)
+        {
+            Logger.Log("on slope", LogLevel.DEBUG, LogType.HARVESTER);
+            if (rb.velocity.magnitude > moveSpeed)
+                rb.velocity = rb.velocity.normalized * moveSpeed;
         }
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            }
+        }
+
     }
- }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f, whatIsGround))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+    }
+}
