9453c60393d39777c3ad912ab01ce70c96c88952
ManuelIni <113027541+ManuelIni@users.noreply.github.com>
2025-04-13 19:11:09 +0200

    reworked pulse to use spherecast instead of scaling collider

--- Assets/BlightProtocol/Scripts/Cockpit/Radar.cs
+++ Assets/BlightProtocol/Scripts/Cockpit/Radar.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Radar : MonoBehaviour
@@ -17,15 +19,17 @@ public class Radar : MonoBehaviour
     public float pulseSeismoEmission = 5f;
 
     // Collider & Sprite
-    public Transform pulseTransform;
+    public Transform pulseSpriteTransform;
     private SpriteRenderer pulseSpriteRenderer;
+
+    public float signatureDelay = 0.01f;
 
     // Duration
 
     [Header("Pulse Range")]
     public float revealRangeFactor = 1f;
     public float revealStartRangeFactor = 1f;
-    
+
 
     [Header("Pulse Strength")]
     // Strength
@@ -35,8 +39,6 @@ public class Radar : MonoBehaviour
     // Speed
     public float revealSpeedFactor = 1f;
     public AnimationCurve pulseSpeedCurve;
-
-    private List<Collider> colliderList = new List<Collider>();
 
     void Awake()
     {
@@ -49,7 +51,7 @@ public class Radar : MonoBehaviour
             Instance = this;
         }
 
-        pulseSpriteRenderer = pulseTransform.GetComponent<SpriteRenderer>();
+        pulseSpriteRenderer = pulseSpriteTransform.GetComponent<SpriteRenderer>();
         mapRevealer = GetComponentInChildren<MapRevealer>();
         InitiateMapRevealer();
     }
@@ -65,8 +67,32 @@ public class Radar : MonoBehaviour
         if (ResourceHandler.Instance.CheckResource(radarData.pulseCostResource) > radarData.pulseCost)
         {
             ResourceHandler.Instance.ConsumeResource(radarData.pulseCostResource, radarData.pulseCost, false, 1f);
-            StartCoroutine(PulseEffect(modifier));
-            mapRevealer.Pulse(radarData.pulseStartRange * revealStartRangeFactor, radarData.pulseRange * revealRangeFactor * modifier, radarData.pulseSpeed * revealSpeedFactor, radarData.pulseDuration);
+
+            Collider[] hitColliders = new Collider[100];
+            int numColliders = Physics.OverlapSphereNonAlloc(
+                transform.position,
+                radarData.pulseRange * modifier,
+                hitColliders,
+                layerMask
+            );
+
+            if (numColliders > 0)
+            {
+                var sortedColliders = hitColliders
+                    .Take(numColliders)
+                    .OrderBy(c => (transform.position - c.transform.position).sqrMagnitude)
+                    .ToArray();
+
+                StartCoroutine(DisplayPulseMarkers(sortedColliders, modifier));
+                StartCoroutine(DisplayPulseRing(modifier));
+            }
+
+            mapRevealer.Pulse(
+                radarData.pulseStartRange * revealStartRangeFactor,
+                radarData.pulseRange * revealRangeFactor * modifier,
+                radarData.pulseSpeed * revealSpeedFactor,
+                radarData.pulseDuration
+            );
 
             Seismograph.Instance.SetOtherEmission("Radar Pulse", pulseSeismoEmission, 1f);
         }
@@ -76,35 +102,48 @@ public class Radar : MonoBehaviour
         }
     }
 
-    private IEnumerator PulseEffect(float modifier)
+    private IEnumerator DisplayPulseMarkers(Collider[] colliders, float modifier)
     {
-        float timer = 0f;
-        float linearTimer = 0f;
+        float maxDistance = radarData.pulseRange * modifier;
+        float pulseDuration = radarData.pulseDuration;
+        float startTime = Time.time;
 
-        float range = radarData.pulseRange * modifier;
-
-        while (timer < radarData.pulseDuration)
+        foreach (var collider in colliders)
         {
-            linearTimer += Time.deltaTime;
+            if (collider == null) continue;
 
-            float currentRadius = Mathf.Lerp(radarData.pulseStartRange, range, timer / radarData.pulseDuration);
-            float currentStrength = pulseStrengthCurve.Evaluate(linearTimer / radarData.pulseDuration);
-            float currentSpeed = pulseSpeedCurve.Evaluate(linearTimer / radarData.pulseDuration) * radarData.pulseSpeed;
+            float distance = Vector3.Distance(transform.position, collider.transform.position);
+            float normalizedDistance = distance / maxDistance;
+            float delay = normalizedDistance * pulseDuration;
 
-            pulseTransform.localScale = Vector3.one * currentRadius;
-            pulseSpriteRenderer.color = new Color(pulseSpriteRenderer.color.r, pulseSpriteRenderer.color.g, pulseSpriteRenderer.color.b, currentStrength);
+            // Wait until the pulse wave would reach this distance
+            while (Time.time - startTime < delay)
+            {
+                yield return null;
+            }
 
-            timer += Time.deltaTime * currentSpeed;
-            yield return null;
+            if (collider.TryGetComponent<EnergySignature>(out EnergySignature signature))
+            {
+                Map.Instance.SetEnergySignature(new Vector3(collider.transform.position.x, 0, collider.transform.position.z), signature);
+            }
+
+
+            yield return new WaitForSeconds(signatureDelay);
         }
-        pulseTransform.localScale = Vector3.one;
     }
+    private IEnumerator DisplayPulseRing(float modifier) {
+        pulseSpriteRenderer.enabled = true;
+        float elapsedTime = 0f;
+        while (elapsedTime < radarData.pulseDuration) {
+            float normalizedTime = elapsedTime / radarData.pulseDuration;
+            float currentSpeed = pulseSpeedCurve.Evaluate(normalizedTime) * radarData.pulseSpeed;
+            float currentStrength = pulseStrengthCurve.Evaluate(normalizedTime / radarData.pulseDuration);
 
-    void OnTriggerEnter(Collider other)
-    {
-        Vector3 collisionPos = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
-        if (other.TryGetComponent<EnergySignature>(out EnergySignature signature)) {
-            Map.Instance.SetEnergySignature(collisionPos, signature);
+            pulseSpriteTransform.localScale = Vector3.one * Mathf.Lerp(radarData.pulseStartRange, radarData.pulseRange * modifier, normalizedTime); 
+            pulseSpriteRenderer.color = new Color(pulseSpriteRenderer.color.r, pulseSpriteRenderer.color.g, pulseSpriteRenderer.color.b, currentStrength);
+            
+            elapsedTime += Time.deltaTime * currentSpeed;
+            yield return null;
         }
     }
 }
