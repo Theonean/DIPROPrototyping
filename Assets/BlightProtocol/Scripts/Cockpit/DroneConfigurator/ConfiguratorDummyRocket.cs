76192cba6233ac9005636ae3127f5007ee930996
ManuelIni <113027541+ManuelIni@users.noreply.github.com>
2025-06-02 11:16:49 +0200

    prepares ui for new component

--- Assets/BlightProtocol/Scripts/Cockpit/DroneConfigurator/ConfiguratorDummyRocket.cs
+++ Assets/BlightProtocol/Scripts/Cockpit/DroneConfigurator/ConfiguratorDummyRocket.cs
@@ -2,20 +2,26 @@ using UnityEngine;
 
 public class ConfiguratorDummyRocket : MonoBehaviour
 {
+    [SerializeField] private bool singleComponent = false;
     private GameObject frontComponent, bodyComponent, propulsionComponent;
-    [SerializeField] private float yOffset = 0f;
+    [SerializeField] private float yOffsetFront, yOffsetBody, yOffsetProp = 0f;
     [SerializeField] private float scaleModifier = 1f;
-    [SerializeField] bool setLayer = false;
 
     public void SetComponent(RocketComponentType type, GameObject newComponent)
     {
+        if (singleComponent)
+        {
+            if (frontComponent != null) Destroy(frontComponent);
+            if (bodyComponent != null) Destroy(bodyComponent);
+            if (propulsionComponent != null) Destroy(propulsionComponent);
+        }
         switch (type)
         {
             case RocketComponentType.FRONT:
                 if (frontComponent != null) Destroy(frontComponent);
                 frontComponent = Instantiate(newComponent, transform, false);
                 frontComponent.transform.localScale *= scaleModifier;
-                frontComponent.transform.localPosition = new Vector3(0, -yOffset, 0);
+                frontComponent.transform.localPosition = new Vector3(0, yOffsetFront, 0);
                 frontComponent.transform.localRotation = Quaternion.identity;
                 frontComponent.layer = gameObject.layer;
                 break;
@@ -23,7 +29,7 @@ public class ConfiguratorDummyRocket : MonoBehaviour
                 if (bodyComponent != null) Destroy(bodyComponent);
                 bodyComponent = Instantiate(newComponent, transform, false);
                 bodyComponent.transform.localScale *= scaleModifier;
-                bodyComponent.transform.localPosition = Vector3.zero;
+                bodyComponent.transform.localPosition = new Vector3(0, -yOffsetBody, 0);
                 bodyComponent.transform.localRotation = Quaternion.identity;
                 bodyComponent.layer = gameObject.layer;
                 break;
@@ -31,7 +37,7 @@ public class ConfiguratorDummyRocket : MonoBehaviour
                 if (propulsionComponent != null) Destroy(propulsionComponent);
                 propulsionComponent = Instantiate(newComponent, transform, false);
                 propulsionComponent.transform.localScale *= scaleModifier;
-                propulsionComponent.transform.localPosition = new Vector3(0, yOffset, 0);
+                propulsionComponent.transform.localPosition = new Vector3(0, yOffsetProp, 0);
                 propulsionComponent.transform.localRotation = Quaternion.identity;
                 propulsionComponent.layer = gameObject.layer;
                 break;
