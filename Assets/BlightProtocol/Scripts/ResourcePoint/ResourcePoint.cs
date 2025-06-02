3626f1abddcfe02a33830fa9e2195205f82d2f68
ManuelIni <113027541+ManuelIni@users.noreply.github.com>
2025-06-02 20:08:50 +0200

    adds images to howToWin UI, removes smoke from Resource Points when starting to harvest

--- Assets/BlightProtocol/Scripts/ResourcePoint/ResourcePoint.cs
+++ Assets/BlightProtocol/Scripts/ResourcePoint/ResourcePoint.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class ResourcePoint : MonoBehaviour
 {
     public int resourceAmount = 100;
     private float gasHarvested = 0f;
 
+    [SerializeField] private VisualEffect[] smokeVFX;
+
     public bool HarvestResource(float amount)
     {
+        if (smokeVFX[0] != null && smokeVFX[0].HasAnySystemAwake())
+        {
+            foreach (var effect in smokeVFX)
+            {
+                effect.Stop();
+            }
+        }
         if (resourceAmount <= 0f)
             return false;
 
         gasHarvested += amount;
 
-        if(gasHarvested > 1f)
+        if (gasHarvested > 1f)
         {
             int gasInt = (int)gasHarvested;
             gasHarvested -= gasInt;
